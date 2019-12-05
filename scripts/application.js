ea3d24155357072323db2613804bd35f14d7b9fa
Sheila Leveille <sbleveille@gmail.com>
2019-12-05 13:49:55 +1100

    updated the smooth scroll effect for IOS

--- scripts/application.js
+++ scripts/application.js
@@ -1,6 +1,6 @@
-// import smoothscroll from 'smoothscroll-polyfill';
-
-// smoothscroll.polyfill();
-
 import { initScrollingNavs } from './plugins/scrolling.js';
 initScrollingNavs();
+
+import { initActiveNavLinks } from './plugins/sidebar.js';
+initActiveNavLinks();
+
